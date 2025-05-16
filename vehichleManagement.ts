c38f398118fc1168ff8464bc6120b590dcbf36ba
Ilysse200 <ilysseted500@gmail.com>
2025-05-16 11:56:12 +0200

    Added id generation in Vehicle management

--- vehichleManagement.ts
+++ vehichleManagement.ts
@@ -11,16 +11,17 @@ enum transmission{
 }
 
 interface vehicle{
-    id:number,
+    id:number, //Id can be optional ?
     plateNumber:string,
     model:string,
-    transmissionType:transmission.AUTOMATIC
+    transmissionType:transmission
 }
 
 class vehicleManagement{
 
     public vehicles:vehicle[] = [];
     addVehicle(vehicl:vehicle):void{
+        
         //Check whether the vehicle exist before adding it
         if(this.isVehichleAvailable(vehicl.plateNumber)){
             console.log(`vehicle with plate number: ${vehicl.plateNumber} already exists \n`);
@@ -29,10 +30,15 @@ class vehicleManagement{
         }
         else{
             //Add the vehicle
+            vehicl.id =this.generateId();
             this.vehicles.push(vehicl);
             console.log(`The vehiche with plate number: ${vehicl.plateNumber} \n`);       
         }
 
+    }
+
+    generateId():number{
+        return this.vehicles.length+1;
     }
 
     isVehichleAvailable(identifier: string):boolean{
@@ -47,14 +53,44 @@ class vehicleManagement{
             this.vehicles[index].model=model;//Replace the vehichle with the value of the new model in the array;
         }
         return `Vehichle updated successfully`;
+        
+    }
+
+    removeVehichles(): void{
+        //Function to remove vehichles
     }
 
     displayVehicles():void{
         //Check if vehicles are present in the database
 
         for(let a:number=0; a<this.vehicles.length;a++){
+            console.log(`Vehicle with id ${this.vehicles[a].id} has model: ${this.vehicles[a].model}`);
+            "\n";
             
         }
 
     }
 }
+
+const vehicleExample:vehicleManagement = new vehicleManagement();
+let v:number=0;
+
+let vehichle1:vehicle = {id:v, plateNumber:'RAA567S', model:'TOYOTA', transmissionType:transmission.AUTOMATIC};
+// v = vehicleExample.generateId(); This is a lot of work
+let vehichle2:vehicle = {id:v, plateNumber:'RAA568L', model:'HONDA', transmissionType:transmission.MNUAL};
+let vehichle3:vehicle = {id:v, plateNumber:'RAA567M', model:'FORD', transmissionType:transmission.AUTOMATIC};
+
+//Add vehichles
+vehicleExample.addVehicle(vehichle1);
+vehicleExample.addVehicle(vehichle2);
+vehicleExample.addVehicle(vehichle3);
+
+//Display the vehichles
+vehicleExample.displayVehicles();
+
+//Check if update works
+vehicleExample.updateVehichleDetails('RAA568L','Nissan');
+
+//Display after updating
+// vehicleExample.displayVehicles();
+
